d2ae5faaa3c0a50a594c2dba3f1875b926999aa9
Daniel Nunes <danielnunespk@gmail.com>
2025-05-05 09:53:24 -0300

    feat: create infra security

--- src/main/java/com/danieln/mscursos/infra/TokenService.java
+++ src/main/java/com/danieln/mscursos/infra/TokenService.java
@@ -1,5 +1,33 @@
 package com.danieln.mscursos.infra;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
 public class TokenService {
+
+    @Value("${api.secret.key}")
+    private String secret;
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            return "Erro ao decodificar o token";
+        }
+    }
+
+
+
+
 
 }
