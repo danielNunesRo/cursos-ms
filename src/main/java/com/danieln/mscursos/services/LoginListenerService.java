088c3780a50cc596f830d7ac84b56762392e56ee
Daniel Nunes <danielnunespk@gmail.com>
2025-05-06 08:19:02 -0300

    feat: create loginlistenerservice

---
+++ src/main/java/com/danieln/mscursos/services/LoginListenerService.java
@@ -0,0 +1,20 @@
+package com.danieln.mscursos.services;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginListenerService {
+
+    private String token;
+
+    @RabbitListener(queues = "${rabbitmq.queue.login.cursos}")
+    public void tokenConsumer(String tokenRecebido) {
+        this.token = tokenRecebido;
+        System.out.println("Token recebido com sucesso");
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+}
